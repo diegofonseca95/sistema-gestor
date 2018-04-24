0502cb04d1bf0abe2c399abbf743d29c446d5bf0
diegofonseca95 <38635960+diegofonseca95@users.noreply.github.com>
2018-04-23 20:24:12 -0500

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+	$tasks = App\Usuario::all();
+	return $tasks;
+
+    //return view('welcome');
+});
+
+Route::get('/', 'UsuariosControlador@index');
+Route::get('/agregarUsuario', 'UsuariosControlador@agregarUsuario');
+Route::post('/agregarUsuario', 'UsuariosControlador@agregarUsuarioBD');
+Route::post('/obtenerUsuario', 'UsuariosControlador@obtenerUsuario');
